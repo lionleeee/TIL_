e5ad4b671254d817cb29679a6b7aa306e7dbf326
lionleeee <lee97hun3@gmail.com>
2023-09-11 10:06:50 +0900

    image 복구

--- documents/.vuepress/const.js
+++ documents/.vuepress/const.js
@@ -1,17 +1,11 @@
 
+exports.DailyCodingList = [
+];
 
 exports.AngularList = [
-  "Angular/123"
 ];
 
 exports.GitList = [
-  "Git/git-guide",
-  "Git/git-ignore",
-  "Git/git-reverse-commit",
-  "Git/pro-git",
-  "Git/rebase-with-sourcetree",
-  "Git/trouble-shooting-on-console",
-  "Git/rebase-vs-merge-commit",
-  "Git/revert-certain-file",
+
 ];
 exports.BundlerList = [];
